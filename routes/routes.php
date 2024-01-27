83061f4b51ee041e1fa46657104a0619737e7a4a
m15studio <123408679+m15studio@users.noreply.github.com>
2024-01-27 10:06:31 +0100

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -2,7 +2,12 @@
 use Illuminate\Support\Facades\Route;
 use M15Studio\LaraGroups\Controllers\GruppoController;
 
-Route::middleware(['role:admin'])->group(function () {
-    Route::get('/gruppo/dashboard', [GruppoController::class, 'index'])->name('gruppo-dashboard');
-    Route::get('/gruppo/{id_gruppo}', [GruppoController::class, 'show'])->name('gruppo-scheda');
+Route::middleware([
+    'auth',
+    'verified',
+])->group(function () {
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/gruppo/dashboard', [GruppoController::class, 'index'])->name('gruppo-dashboard');
+        Route::get('/gruppo/{id_gruppo}', [GruppoController::class, 'show'])->name('gruppo-scheda');
+    });
 });
