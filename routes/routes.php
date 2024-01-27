047f6cfd68bf4510ee324054cb17b55143b1369a
m15studio <123408679+m15studio@users.noreply.github.com>
2024-01-27 09:31:19 +0100

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -2,9 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use M15Studio\LaraGroups\Controllers\GruppoController;
 
-Route::middleware(['auth', config('jetstream.auth_session'), 'locale'])->group(function () {
-    Route::controller(GruppoController::class)->group(function () {
-        Route::get('/gruppo/dashboard', 'index')->name('gruppo-dashboard');
-        Route::get('/gruppo/{lotto}', 'show')->name('gruppo-scheda');
-    });
+Route::controller(GruppoController::class)->group(function () {
+    Route::get('/gruppo/dashboard', 'index')->name('gruppo-dashboard');
+    Route::get('/gruppo/{lotto}', 'show')->name('gruppo-scheda');
 });
